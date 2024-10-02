db5b87a3db4e66a2e48e5b2a6b6d203328026655
Gokefleak <mihailpodoplelov8@gmail.com>
2024-10-02 11:56:51 +0300

    add .env

--- main.py
+++ main.py
@@ -5,3 +5,33 @@ import datetime
 from telebot import types
 import io
 import os
+user_data = {}
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = telebot.TeleBot(os.getenv("TELEGRAM_BOT_TOKEN"))
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    chat_id = message.chat.id
+    if not (chat_id in user_data):
+        user_data[chat_id] = {
+            'message_id': 0,
+
+        }
+    markup = types.InlineKeyboardMarkup()
+    buttonout1 = types.InlineKeyboardButton('Войти в аккаунт админестратора 🔐', callback_data='entrance')
+    buttonout2 = types.InlineKeyboardButton('Рассписание', callback_data='timetable')
+    buttonout3 = types.InlineKeyboardButton('Узнать свой ID 🔍', callback_data='myId')
+    row1 = [buttonout1]
+    row2 = [buttonout2]
+    row3 = [buttonout3]
+    markup.row(*row1)
+    markup.row(*row2)
+    markup.row(*row3)
+    bot.send_message(chat_id=message.chat.id, text='Перед использованием авторизируйтесь', reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: call.data == 'timetable')
+def timetable(call):
+    chat_id = call.message.chat.id
+bot.polling()
